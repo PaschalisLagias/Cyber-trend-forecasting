09326b76216af6d7630b13c75f308b4d90f78a21
PaschalisLagias <paschalis.lagias@gmail.com>
2025-05-31 22:28:20 +0100

    Removed unused imports

--- Data_Preparation/Twitter_Tweets/ACA.py
+++ Data_Preparation/Twitter_Tweets/ACA.py
@@ -17,20 +17,6 @@ import requests
 # when creating & adding to the dataset
 import os
 
-# For dealing with json responses we receive from the API
-import json
-
-# For displaying the data after
-import pandas as pd
-
-# For saving the response data in CSV format
-import csv
-
-# For parsing the dates received from twitter in readable formats
-import datetime
-import dateutil.parser
-import unicodedata
-
 # To add wait time between requests
 import time
 from csv import writer
