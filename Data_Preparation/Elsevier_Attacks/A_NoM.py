6406c7b46d61831fecc50f748b47fa2edd816b8c
PaschalisLagias <paschalis.lagias@gmail.com>
2025-08-09 01:00:42 +0100

    Re-formatted Elsevier scripts.

--- Data_Preparation/Elsevier_Attacks/A_NoM.py
+++ Data_Preparation/Elsevier_Attacks/A_NoM.py
@@ -1,275 +1,285 @@
-from elsapy.elsclient import ElsClient
-from elsapy.elsprofile import ElsAuthor, ElsAffil
-from elsapy.elsdoc import FullDoc, AbsDoc
-from elsapy.elssearch import ElsSearch
+import time
 import json
-import sys
-import csv
-import requests
-from bs4 import BeautifulSoup
+
+from elsapy.elsclient import ElsClient
+from elsapy.elssearch import ElsSearch
+
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions
 from selenium.common.exceptions import TimeoutException
-import time
-import pandas as pd
+
 from webdriver_manager.firefox import GeckoDriverManager
 
+# Load Elsevier API key
+with open("config.json") as config_file:
+    config = json.load(config_file)
 
+# Initialise client
+client = ElsClient(config["apikey"])
 
-   
-## Load configuration
-con_file = open("config.json")#Place your Elsevier key in this file
-config = json.load(con_file)
-con_file.close()
-
-## Initialise client
-client = ElsClient(config['apikey'])
-
-#harvester config (used to count the number of mentions)
+# harvester config (used to count the number of mentions)
 options = Options()
-options.add_argument('--headless')
-driver = webdriver.Firefox(options=options, service=Service(GeckoDriverManager().install()))#executable_path = 'C:/Program Files/Mozilla Firefox/geckodriver')
+options.add_argument("--headless")
 
+# executable_path = 'C:/Program Files/Mozilla Firefox/geckodriver')
+driver = webdriver.Firefox(
+    options=options,
+    service=Service(GeckoDriverManager().install())
+)
 
-
-# this list will be used in the query of Elsevier (attack list)
-attack_list=['"Denial-of-Service Attack" OR {DDoS}',
-'Phishing',
-'Ransomware',
-'Password Attack',
-'"SQL Injection"',
-'("Account Hijack*") OR ("Account Takeover")',
-'Website Deface*',
-'Trojan Attack', 
-'Vulnerabilit* Attack exploit*', 
-'"Zero-day" AND Attack',
-'"Advanced Persistent Threat"',
-'"cross-site scripting"',
-'Malware OR TROJAN OR SPYWARE OR MALVERTIS* OR RANSOMWARE OR ("COMPUTER VIRUS") OR (WORM ATTACK) OR (KEYLOG* Attack) OR (KEYSTROKE LOG Attack) OR (MALICIOUS CODE) OR (MALICIOUS SOFTWARE) OR ADWARE OR ROOTKIT OR BOTNET OR (BACKDOOR ATTACK)',
-'"Data Breach" OR "Data Leak*" OR "information leak*" OR "Data SPILL" OR "unintentional information disclos*"', 
-'(Disinformation spread) OR (Misinformation spread) OR ("false information" spread) OR ("misleading information" spread) OR Deepfake',
-'"Targeted Attack"',
-'Adware',
-'"Brute Force" Attack',
-'Malvertis*',
-'Backdoor Attack',
-'Botnet',
-'Cryptojack*',
-'Worm Attack',
-'Spyware',
-'"Man-in-the-middle" OR {MITM}',
-'"DNS Spoof*" OR "DNS cache poison*"',
-'Pegasus Spyware',
-'CoolWebSearch',
-'Gator GAIN',
-'"180search Assistant"',
-'Transponder *vx2*',
-'WannaCry AND (Ransomware OR Attack)',
-'"Colonial Pipeline" AND (Ransomware OR Attack)',
-'Cryptolocker OR Crypto-locker',
-'Dropper Trojan',
-'Wiper malware',
-'Pharming Attack',
-'"Insider Threat"',
-'(Drive-by Download) OR (Drive-by Install)',
-'Rootkit',
-'Adversarial Attack',
-'"Data Poisoning"',
-'Deepfake',
-'Deeplocker OR (Deep-locker) OR "Deep locker"',
-'"Supply Chain" AND Attack',
-'IoT Device Attack',
-'(KEYLOG* Attack) OR (KEYSTROKE LOG Attack)',
-'"DNS Tunnel*"',
-'"Session Hijacking" OR "cookie hijacking" OR "cookie poisoning"',
-'URL Attack',
-'"Unknown Attack" AND (Security OR Cyber*)'
+# List to query Elsevier (attack list)
+attack_list = [
+    '"Denial-of-Service Attack" OR {DDoS}',
+    'Phishing',
+    'Ransomware',
+    'Password Attack',
+    '"SQL Injection"',
+    '("Account Hijack*") OR ("Account Takeover")',
+    'Website Deface*',
+    'Trojan Attack',
+    'Vulnerabilit* Attack exploit*',
+    '"Zero-day" AND Attack',
+    '"Advanced Persistent Threat"',
+    '"cross-site scripting"',
+    'Malware OR TROJAN OR SPYWARE OR MALVERTIS* OR RANSOMWARE OR ("COMPUTER VIRUS") OR (WORM ATTACK) OR (KEYLOG* Attack) OR (KEYSTROKE LOG Attack) OR (MALICIOUS CODE) OR (MALICIOUS SOFTWARE) OR ADWARE OR ROOTKIT OR BOTNET OR (BACKDOOR ATTACK)',
+    '"Data Breach" OR "Data Leak*" OR "information leak*" OR "Data SPILL" OR "unintentional information disclos*"',
+    '(Disinformation spread) OR (Misinformation spread) OR ("false information" spread) OR ("misleading information" spread) OR Deepfake',
+    '"Targeted Attack"',
+    'Adware',
+    '"Brute Force" Attack',
+    'Malvertis*',
+    'Backdoor Attack',
+    'Botnet',
+    'Cryptojack*',
+    'Worm Attack',
+    'Spyware',
+    '"Man-in-the-middle" OR {MITM}',
+    '"DNS Spoof*" OR "DNS cache poison*"',
+    'Pegasus Spyware',
+    'CoolWebSearch',
+    'Gator GAIN',
+    '"180search Assistant"',
+    'Transponder *vx2*',
+    'WannaCry AND (Ransomware OR Attack)',
+    '"Colonial Pipeline" AND (Ransomware OR Attack)',
+    'Cryptolocker OR Crypto-locker',
+    'Dropper Trojan',
+    'Wiper malware',
+    'Pharming Attack',
+    '"Insider Threat"',
+    '(Drive-by Download) OR (Drive-by Install)',
+    'Rootkit',
+    'Adversarial Attack',
+    '"Data Poisoning"',
+    'Deepfake',
+    'Deeplocker OR (Deep-locker) OR "Deep locker"',
+    '"Supply Chain" AND Attack',
+    'IoT Device Attack',
+    '(KEYLOG* Attack) OR (KEYSTROKE LOG Attack)',
+    '"DNS Tunnel*"',
+    '"Session Hijacking" OR "cookie hijacking" OR "cookie poisoning"',
+    'URL Attack',
+    '"Unknown Attack" AND (Security OR Cyber*)'
 ]
 
+# List for counting mentions per attack type by the harvester (attack keywords)
+mention_list = [
+    ('Denial-of-Service attack', 'DDoS'),
+    ('Phishing',),
+    ('Ransomware',),
+    ('Password',),
+    ('SQL Injection', 'SQLI'),
+    ('Account Hijack', 'Account Takeover'),
+    ('Deface',),
+    ('Trojan',),
+    ('Vulnerability',),
+    ('Zero-day',),
+    ('Advanced Persistent Threat', 'APT Attack'),
+    ('cross-site scripting',),
+    ('Malware', 'TROJAN', 'SPYWARE', 'MALVERTIS',  'RANSOMWARE', 'VIRUS',
+     'WORM', 'KEYLOG', 'KEYSTROKE LOG', 'MALICIOUS CODE', 'MALICIOUS SOFTWARE',
+     'ADWARE', 'ROOTKIT', 'BOTNET', 'BACKDOOR'),
+    ('Data Breach', 'Data Leak', 'information leak',
+     'Data SPILL', 'unintentional information disclos'),
+    ('Disinformation', 'Misinformation', 'false information',
+     'misleading information', 'Deepfake'),
+    ('Targeted Attack',),
+    ('Adware',),
+    ('Brute Force',),
+    ('Malvertis',),
+    ('Backdoor',),
+    ('Botnet',),
+    ('Cryptojack',),
+    ('Worm',),
+    ('Spyware',),
+    ('Man-in-the-middle', 'MITM'),
+    ('DNS Spoof', 'DNS cache poison'),
+    ('Pegasus',),
+    ('CoolWebSearch',),
+    ('Gator Spyware', 'GAIN Spyware', 'GAIN) Spyware',
+     'Gator (GAIN)', 'Gator/GAIN', 'Gator GAIN'),
+    ('180search Assistant',),
+    ('Transponder vx2', 'Transponder Spyware', 'vx2 Spyware', 'vx2) Spyware',
+     'Transponder/vx2', 'Transponder (vx2)'),
+    ('WannaCry',),
+    ('Colonial Pipeline',),
+    ('Cryptolocker', 'Crypto-locker'),
+    ('Dropper',),
+    ('Wiper',),
+    ('Pharming',),
+    ('Insider Threat',),
+    ('Drive-by',),
+    ('Rootkit',),
+    ('Adversarial Attack',),
+    ('Data Poisoning',),
+    ('Deepfake',),
+    ('Deeplocker', 'Deep locker'),
+    ('Supply Chain',),
+    ('IoT Device', 'IoT Attack', 'Attack on IoT', 'Attack the IoT'),
+    ('KEYLOGGER', 'KEYSTROKE LOG'),
+    ('DNS Tunnel',),
+    ('Session Hijack', 'Hijack Session', 'Hijack the Session',
+     'cookie hijack', 'cookie poison'),
+    ('URL manipul', 'URL tamper', 'URL interpret', 'URL Attack'),
+    ('Unknown Attack',)
+]
 
-# this list will be used to count the number of mentions of each attack type by the harvester (attack keywords list)
-mention_list=[('Denial-of-Service attack' , 'DDoS'),
-              ('Phishing',),
-              ('Ransomware',),
-              ('Password',),
-              ('SQL Injection','SQLI'),
-              ('Account Hijack','Account Takeover'),
-              ('Deface',),
-              ('Trojan',),
-              ('Vulnerability'),
-              ('Zero-day',),
-              ('Advanced Persistent Threat','APT Attack'),
-              ('cross-site scripting',),
-              ('Malware', 'TROJAN', 'SPYWARE', 'MALVERTIS',  'RANSOMWARE', 'VIRUS', 'WORM', 'KEYLOG', 'KEYSTROKE LOG', 'MALICIOUS CODE', 'MALICIOUS SOFTWARE', 'ADWARE', 'ROOTKIT', 'BOTNET', 'BACKDOOR'),
-              ('Data Breach', 'Data Leak', 'information leak', 'Data SPILL', 'unintentional information disclos'),
-              ('Disinformation', 'Misinformation', 'false information', 'misleading information', 'Deepfake'),
-              ('Targeted Attack',),
-              ('Adware',),
-              ('Brute Force',),
-              ('Malvertis',),
-              ('Backdoor',),
-              ('Botnet',),
-              ('Cryptojack',),
-              ('Worm',),
-              ('Spyware',),
-              ('Man-in-the-middle', 'MITM'), 
-              ('DNS Spoof', 'DNS cache poison'),
-              ('Pegasus',),
-              ('CoolWebSearch',),
-              ('Gator Spyware','GAIN Spyware','GAIN) Spyware','Gator (GAIN)','Gator/GAIN','Gator GAIN'),
-              ('180search Assistant',),
-              ('Transponder vx2','Transponder Spyware','vx2 Spyware','vx2) Spyware','Transponder/vx2', 'Transponder (vx2)'),
-              ('WannaCry',),
-              ('Colonial Pipeline',),
-              ('Cryptolocker', 'Crypto-locker'),
-              ('Dropper',),
-              ('Wiper',),
-              ('Pharming',),
-              ('Insider Threat',),
-              ('Drive-by',),
-              ('Rootkit',),
-              ('Adversarial Attack',),
-              ('Data Poisoning',),
-              ('Deepfake',),
-              ('Deeplocker','Deep locker'),
-              ('Supply Chain',),
-              ('IoT Device','IoT Attack','Attack on IoT','Attack the IoT'),
-              ('KEYLOGGER','KEYSTROKE LOG'),
-              ('DNS Tunnel',),
-              ('Session Hijack','Hijack Session','Hijack the Session', 'cookie hijack', 'cookie poison'),
-              ('URL manipul' , 'URL tamper' , 'URL interpret' , 'URL Attack'),
-              ('Unknown Attack',)
-             ]                       
-              
+years_list = list(range(2011, 2025))
+month_list = ["January", "February", "March", "April", "May", "June", "July",
+              "August", "September", "October", "November", "December"]
 
+empty = "Result set was empty"
+search_results_list = []
 
-
-years_list=list(range(2011,2023));
-month_list=['January','February','March','April','May','June','July','August','September','October','November','December']
-
-empty='Result set was empty'
-
-search_results_list=[]
-
-#Part 1 - collect the URLs of all relevant documents (for each month and each attack type)
-#The search is within the title, abstract, and keywords
+# Part 1: Collect URLs of all relevant documents (per month and attack type)
+# The search is within the title, abstract, and keywords
 for year in years_list:
-    print ("searching in year:", str(year))
-    
+    print("searching in year:", str(year))
     for month in month_list:
+        # Date range: July 2011 - May 2025
+        if any([
+            year == 2011 and month in month_list[:6],
+            year == 2025 and month in month_list[6:]
+        ]):
+            continue
 
-        #Our dataset starts from July 2011
-        if year==2011 and month in month_list[:6]:
-            continue;
+        print("searching in year/month:", str(year), "/", month)
+        slsm = []
+        for index, attack in enumerate(attack_list):
+            slm = []
+            print("searching for ", attack)
+            doc_srch = ElsSearch(
+                "TITLE(" + attack + ") OR ABS(" + attack + ") OR KEY(" + attack + ") AND PUBDATETXT(" + month + " " + str(year) + ")",
+                "scopus"
+            )
 
-        
-        print ("searching in year/month:", str(year),"/",month)
-        slsm=[]
-        for index,attack in enumerate(attack_list):
-            slm=[]
-            print("searching for ",attack)
-            doc_srch = ElsSearch("TITLE("+attack+") OR ABS("+attack+") OR KEY("+attack+") AND PUBDATETXT("+month+" "+str(year)+")",'scopus')
-            doc_srch.execute(client, get_all = True)
-            if(empty in str(doc_srch.results)):
-                #print(doc_srch.results)
-                slsm.append(slm);
+            # TODO: Debugging needed here
+            doc_srch.execute(client, get_all=True)
+            if empty in str(doc_srch.results):
+                # print(doc_srch.results)
+                slsm.append(slm)
                 continue
             else:
-                #print("got" , len(doc_srch.results))
+                # print("got" , len(doc_srch.results))
                 for col in doc_srch.results:
                     slm.append(col)
-                slsm.append(slm);
+                slsm.append(slm)
         search_results_list.append(slsm)
-        
-  
 
-
-
-
-#Part 2 - Harvesting and counting the number of mentions of each attack type during each month
-#The counting is within the title, abstract, and keywords
-print ("--------------------------Harvesting Time ---------------------------")
-data=[]
-for index,row in enumerate(search_results_list):
-    print("Out of", len(search_results_list)," Harvesting for column ",index)
+# Part 2: Harvest and count number of mentions per attack type for each month
+# The counting is within the title, abstract, and keywords
+print("--------------------------Harvesting Time ---------------------------")
+data = []
+for index, row in enumerate(search_results_list):
+    print("Out of", len(search_results_list), " Harvesting for column ", index)
     attack_map = {}
     for mention in mention_list:
-        attack_map[mention[0]]=0;
+        attack_map[mention[0]] = 0
     for j, col in enumerate(row):
         for k, result in enumerate(col):
-            url=result['link'][2]['@href']
-            print("URL:",url)
+            url = result['link'][2]['@href']
+            print("URL:", url)
             try:
                 driver.get(url)
             except:
                 time.sleep(20)
                 driver.get(url)
             time.sleep(1)
-            delay = 20 # seconds
+            delay = 20  # seconds
             try:
-                myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CLASS_NAME, 'Highlight-module__akO5D')))
+                myElem = WebDriverWait(driver, delay).until(
+                    expected_conditions.presence_of_element_located((
+                        By.CLASS_NAME,
+                        'Highlight-module__akO5D'
+                    )))
+
             except TimeoutException:
-                print ("Loading took too much time!")
-            #click index keywords
-            keyword_elements =  driver.find_elements(By.TAG_NAME,"button")
-            keyword_element  = None
-            while(True):
-                failed=False
-                for element in keyword_elements :
+                print("Loading took too much time!")
+            # click index keywords
+            keyword_elements = driver.find_elements(By.TAG_NAME, "button")
+            keyword_element = None
+            while True:
+                failed = False
+                for element in keyword_elements:
                     try:
-                        if('Indexed keywords' in element.text):
-                            keyword_element=element
+                        if 'Indexed keywords' in element.text:
+                            keyword_element = element
                             print("keyword element found!")
-                            break;
+                            break
                     except:
                         print("button issue")
-                        keyword_element=None
-                        failed=True
+                        keyword_element = None
+                        failed = True
                         time.sleep(1)
-                        break;
-                if failed==False:
-                     break
+                        break
+                if not failed:
+                    break
                 else:
-                     keyword_elements =  driver.find_elements(By.TAG_NAME,"button")
+                    keyword_elements = \
+                        driver.find_elements(By.TAG_NAME, "button")
 
-            if(keyword_element is not None) :       
+            if keyword_element is not None:
                 keyword_element.click()
                 time.sleep(1)
             mention = mention_list[j]
             
-            #counting
-            while (True):
-                failed=False
-                totalQ=0;
-                text_elements =  driver.find_elements(By.CLASS_NAME,"Highlight-module__akO5D");
+            # counting
+            while True:
+                failed = False
+                totalQ = 0
+                text_elements = driver.find_elements(
+                    By.CLASS_NAME,
+                    "Highlight-module__akO5D"
+                )
+
                 for element in text_elements:
                     try:
-                        element_text=element.text
+                        element_text = element.text
                     except:
-                        failed=True;
-                        break;
-                    count = sum(element_text.replace("-"," ").upper().count(x.upper().replace("-"," ")) for x in mention)
-                    print("count:",count)
-                    totalQ+=count
-                if(not failed):
-                    break;
-            attack_map[mention[0]]+=totalQ
-            print("for attack",mention,"sum is now:",  attack_map[mention[0]])
-    data_list=[]
+                        failed = True
+                        break
+                    count = sum(element_text.replace("-", " ").upper().count(x.upper().replace("-"," ")) for x in mention)
+                    print("count:", count)
+                    totalQ += count
+                if not failed:
+                    break
+            attack_map[mention[0]] += totalQ
+            print("for attack", mention, "sum is now:",  attack_map[mention[0]])
+    data_list = []
     for key in attack_map:
-        print(key,": ",attack_map[key])
+        print(key, ": ", attack_map[key])
         data_list.append(attack_map[key])
     data.append(data_list)  
     
-    #save data to file
-    file_object = open('Attacks_NoM.txt', 'a')
-    for ii,item in enumerate(data_list):
+    # save data to file
+    file_object = open("Attacks_NoM_V2.txt", "a")
+    for ii, item in enumerate(data_list):
         file_object.write(str(item))
-        if ii<len(data_list)-1:
+        if ii < len(data_list)-1:
             file_object.write("\t")
     file_object.write("\n")
     file_object.close()
