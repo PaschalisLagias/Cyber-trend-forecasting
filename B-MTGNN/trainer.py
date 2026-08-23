3e6cabbb329ee4d144e0f0c362e819e91accb673
PaschalisLagias <paschalis.lagias@gmail.com>
2026-08-23 13:19:51 +0100

    Fixed trainer.py formatting.

--- B-MTGNN/trainer.py
+++ B-MTGNN/trainer.py
@@ -1,13 +1,22 @@
+import torch
 import torch.optim as optim
-import math
-from net import *
+# import math
+
 import util
+# from net import *
+
+
 class Trainer():
-    def __init__(self, model, lrate, wdecay, clip, step_size, seq_out_len, scaler, device, cl=True):
+    def __init__(
+        self,
+        model, lrate, wdecay, clip, step_size,
+        seq_out_len, scaler, device, cl: bool = True
+    ):
         self.scaler = scaler
         self.model = model
         self.model.to(device)
-        self.optimizer = optim.Adam(self.model.parameters(), lr=lrate, weight_decay=wdecay)
+        self.optimizer = optim.Adam(self.model.parameters(),
+                                    lr=lrate, weight_decay=wdecay)
         self.loss = util.masked_mae
         self.clip = clip
         self.step = step_size
@@ -16,17 +25,24 @@ class Trainer():
         self.seq_out_len = seq_out_len
         self.cl = cl
 
-    def train(self, input, real_val, idx=None):
+    def train(self, input_, real_val, idx=None):
         self.model.train()
         self.optimizer.zero_grad()
-        output = self.model(input, idx=idx)
-        output = output.transpose(1,3)
-        real = torch.unsqueeze(real_val,dim=1)
+
+        output = self.model(input_, idx=idx)
+        output = output.transpose(1, 3)
+
+        real = torch.unsqueeze(real_val, dim=1)
         predict = self.scaler.inverse_transform(output)
-        if self.iter%self.step==0 and self.task_level<=self.seq_out_len:
+
+        if self.iter % self.step == 0 and self.task_level <= self.seq_out_len:
             self.task_level +=1
         if self.cl:
-            loss = self.loss(predict[:, :, :, :self.task_level], real[:, :, :, :self.task_level], 0.0)
+            loss = self.loss(
+                predict[:, :, :, :self.task_level],
+                real[:, :, :, :self.task_level], 0.0
+            )
+
         else:
             loss = self.loss(predict, real, 0.0)
 
@@ -36,40 +52,51 @@ class Trainer():
             torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.clip)
 
         self.optimizer.step()
+
         # mae = util.masked_mae(predict,real,0.0).item()
         mape = util.masked_mape(predict,real,0.0).item()
         rmse = util.masked_rmse(predict,real,0.0).item()
         self.iter += 1
         return loss.item(),mape,rmse
 
-    def eval(self, input, real_val):
+    def eval(self, input_, real_val):
         self.model.eval()
-        output = self.model(input)
-        output = output.transpose(1,3)
-        real = torch.unsqueeze(real_val,dim=1)
-        predict = self.scaler.inverse_transform(output)
-        loss = self.loss(predict, real, 0.0)
-        mape = util.masked_mape(predict,real,0.0).item()
-        rmse = util.masked_rmse(predict,real,0.0).item()
-        return loss.item(),mape,rmse
 
+        output = self.model(input_)
+        output = output.transpose(1, 3)
+
+        real = torch.unsqueeze(real_val, dim=1)
+        predict = self.scaler.inverse_transform(output)
+
+        loss = self.loss(predict, real, 0.0)
+        mape = util.masked_mape(predict, real, 0.0).item()
+        rmse = util.masked_rmse(predict, real, 0.0).item()
+        return loss.item(), mape, rmse
 
 
 class Optim(object):
 
     def _makeOptimizer(self):
         if self.method == 'sgd':
-            self.optimizer = optim.SGD(self.params, lr=self.lr, weight_decay=self.lr_decay)
+            self.optimizer = optim.SGD(self.params, lr=self.lr,
+                                       weight_decay=self.lr_decay)
         elif self.method == 'adagrad':
-            self.optimizer = optim.Adagrad(self.params, lr=self.lr, weight_decay=self.lr_decay)
+            self.optimizer = optim.Adagrad(self.params, lr=self.lr,
+                                           weight_decay=self.lr_decay)
         elif self.method == 'adadelta':
-            self.optimizer = optim.Adadelta(self.params, lr=self.lr, weight_decay=self.lr_decay)
+            self.optimizer = optim.Adadelta(self.params, lr=self.lr,
+                                            weight_decay=self.lr_decay)
         elif self.method == 'adam':
-            self.optimizer = optim.Adam(self.params, lr=self.lr, weight_decay=self.lr_decay)
+            self.optimizer = optim.Adam(self.params, lr=self.lr,
+                                        weight_decay=self.lr_decay)
         else:
             raise RuntimeError("Invalid optim method: " + self.method)
 
-    def __init__(self, params, method, lr, clip, lr_decay=1, start_decay_at=None):
+    def __init__(
+        self,
+        params, method, lr, clip,
+        lr_decay=1, start_decay_at=None
+    ):
         self.params = params  # careful: params may be a generator
         self.last_ppl = None
         self.lr = lr
@@ -99,10 +126,11 @@ class Optim(object):
         # for param in self.params:
         #     if shrinkage < 1:
         #         param.grad.data.mul_(shrinkage)
-        self.optimizer.step()
-        return  grad_norm
 
-    # decay learning rate if val perf does not improve or we hit the start_decay_at limit
+        self.optimizer.step()
+        return grad_norm
+
+    # Decay lrate if val perf does not improve or we hit start_decay_at limit
     def updateLearningRate(self, ppl, epoch):
         if self.start_decay_at is not None and epoch >= self.start_decay_at:
             self.start_decay = True
@@ -112,9 +140,8 @@ class Optim(object):
         if self.start_decay:
             self.lr = self.lr * self.lr_decay
             print("Decaying learning rate to %g" % self.lr)
-        #only decay for one epoch
+
+        # Only decay for one epoch
         self.start_decay = False
-
         self.last_ppl = ppl
-
         self._makeOptimizer()
